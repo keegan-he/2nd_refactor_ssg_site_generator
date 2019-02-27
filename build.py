e6b9ed78ef340b91ed71dd820f291b7bdce5086f
Keegan H <keeganslane@gmail.com>
2019-02-26 21:06:55 -0800

    working

--- build.py
+++ build.py
@@ -1,30 +1,29 @@
 import glob
 import os
 
-"""read in all HTML files"""
-content_html_files = glob.glob("content/*.html")
-print(content_html_files)
+def build_pages():
+        
+    """read in all HTML files"""
+    content_html_files = glob.glob("content/*.html")
+    print(content_html_files)
 
-PAGES = []
+    PAGES = []
 
-for file_path in content_html_files:
-    file_name = os.path.basename(file_path)
-    print(file_name)
-    name_only, extension = os.path.splitext(file_name)
-    print(name_only)
-
-    # TEMPLATE = template = open("templates/base.html").read()
-    with open("templates/base.html", "r") as file:
-        TEMPLATE = file.read()
-
-    PAGES.append({
-        "filename": file_path,
-        "title": file_name,
-        "output": "docs/index.html",
-    })
+    for file_path in content_html_files:
+        file_name = os.path.basename(file_path)
+        print(file_name)
+        name_only, extension = os.path.splitext(file_name)
+        print(name_only)
 
 
-print(PAGES)
+        PAGES.append({
+            "filename": file_path,
+            "title": file_name,
+            "output": "docs/{}".format(file_name),
+        })
+
+    print(PAGES)
+    return PAGES
 
 
 def read_content(filename):
@@ -33,18 +32,22 @@ def read_content(filename):
 
 def apply_template(a_string):
     """ read template file and combine with content  """
+
+    # TEMPLATE = template = open("templates/base.html").read()
+    with open("templates/base.html", "r") as file:
+        TEMPLATE = file.read()
     return TEMPLATE.replace("{{content}}", a_string)  # string replacing
 
 
 def write_content(content, output_filename):
     """ loop through pages and write content to output files """
-    with open(output_filename, "w+") as outfile:
+    with open(output_filename, "w") as outfile:
         outfile.write(content)
 
 
 def main():
     """ invoke write content function """
-    for page in PAGES:
+    for page in build_pages():
         content = read_content(page["filename"])
         templated_content = apply_template(content)
         write_content(templated_content, page["output"])
