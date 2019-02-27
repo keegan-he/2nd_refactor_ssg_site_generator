515d21a841d8c29d4cff8b191014426f0956aef6
Keegan H <keeganslane@gmail.com>
2019-02-26 21:53:47 -0800

    generates pages

--- build.py
+++ build.py
@@ -12,15 +12,13 @@ def build_pages():
 
     for file_path in content_html_files:
         file_name = os.path.basename(file_path)
-        print(file_name)
         name_only, extension = os.path.splitext(file_name)
-        print(name_only)
 
 
         PAGES.append({
             "filename": file_path,
-            "title": file_name,
-            "output": "docs/{}".format(file_name),
+            "title": name_only.title(),
+            "output": file_name,
         })
 
     print(PAGES)
@@ -31,26 +29,28 @@ def read_content(filename):
     return open(filename).read()
 
 
-def apply_template(filename, title):
+def apply_template(filename, title, pages):
     """ read template file and combine with content  """
     index_html = open(filename).read()
     template_html = open("templates/base.html").read()
     template = Template(template_html)
     return template.render(
+        pages=pages,
         title=title,
         content=index_html,
     )
 
 def write_content(content, output_filename):
     """ loop through pages and write content to output files """
-    with open(output_filename, "w") as outfile:
+    with open("docs/{}".format(output_filename), "w") as outfile:
         outfile.write(content)
 
 
 def main():
     """ invoke write content function """
-    for page in build_pages():
-        templated_content = apply_template(page["filename"], page["title"])
+    pages = build_pages()
+    for page in pages:
+        templated_content = apply_template(page["filename"], page["title"], pages)
         write_content(templated_content, page["output"])
 
 
