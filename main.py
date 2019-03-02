41ff237b003471196389787e9e5ffaa101a1029b
Keegan H <keeganslane@gmail.com>
2019-03-01 19:26:53 -0800

    building out main.py

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import sys
+import utils
+
 print("This is argv:", sys.argv)
 command = sys.argv[1]
 print(command)
 if command == "build":
     print("Build was specified")
 # TODO: Do something here... elif command == "new":
+    
     print("New page was specified")
 # TODO: Do something here... else:
     print("Please specify ’build’ or ’new’")
