63346b4fb5ca0ca01f706e0311560efa1379816f
keegan-h <keeganslane@gmail.com>
2019-03-02 17:09:44 -0800

    main.py build working

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 import sys
+import utils
 
 print("This is argv:", sys.argv)
 command = sys.argv[1]
 print(command)
 if command == "build":
     print("Build was specified")
-    import utils
-    utils.build_pages()
+    # import utils
+    utils.main()
+    #utils.build_pages()
 
 elif command == "new":
     print("New page was specified")
-    import utils
+    # import utils
     utils.main()
     utils.build_pages()
 
